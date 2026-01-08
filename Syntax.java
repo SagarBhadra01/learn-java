730470cb3a5646d3c957fe6497f962dc1584a723
SagarBhadra01 <sagarbhadra404@gmail.com>
2026-01-08 23:20:42 +0530

    java basic syntax

---
+++ Syntax.java
@@ -0,0 +1,18 @@
+public class Syntax {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+//Java is case-sensitive. MyClass and myclass would be treated as two completely different names.
+//Every line of code that runs in Java must be inside a class. 
+//The class name should always start with an uppercase first letter.
+//The main() method is required in every Java program. It is where the program starts running
+//The curly braces {} mark the beginning and the end of a block of code.
+//each Java statement must end with a semicolon (;).
+// This is a comment
+/*
+This is a multi-line comment.
+This is useful for explaining larger sections of code.
+*/
+//Java files are saved with a .java extension. The file name must match the class name.
