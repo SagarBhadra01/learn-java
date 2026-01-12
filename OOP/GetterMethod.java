fb0299c2531ebb2f210ce0c3ffd66876b6f3e3a1
SagarBhadra01 <sagarbhadra404@gmail.com>
2026-01-12 19:47:15 +0530

    understand oop getter method

---
+++ OOP/GetterMethod.java
@@ -0,0 +1,28 @@
+package OOP;
+
+public class GetterMethod {
+    private String name;
+    private int age;
+
+    // Constructor
+    public GetterMethod(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Main method to test the class
+    public static void main(String[] args) {
+        GetterMethod person = new GetterMethod("Alice", 30);
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
