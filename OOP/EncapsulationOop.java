9a2c92e5e26eceeb210d3eca8eb7bbfb4fc19449
SagarBhadra01 <sagarbhadra404@gmail.com>
2026-01-13 22:04:31 +0530

    Create EncapsulationOop.java

---
+++ OOP/EncapsulationOop.java
@@ -0,0 +1,41 @@
+package OOP;
+public class EncapsulationOop {
+    public static void main(String[] args) {
+        // Creating a Person object
+        Person person = new Person();
+        
+        // Setting values using setter methods
+        person.setName("John Doe");
+        person.setAge(30);
+        
+        // Getting values using getter methods
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+
+    // Person class with encapsulated fields
+    static class Person {
+        private String name;
+        private int age;
+
+        // Getter for name
+        public String getName() {
+            return name;
+        }
+
+        // Setter for name
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        // Getter for age
+        public int getAge() {
+            return age;
+        }
+
+        // Setter for age
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }   
+}
